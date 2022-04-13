a8026b6990f510469fd78b5b27eb7c6ed3adbb67
ahh1539 <ahh1539@g.rit.edu>
2022-04-13 11:24:06 -0400

    challenge 1

---
+++ copilot-challenges.py
@@ -0,0 +1,12 @@
+import json
+import requests
+
+# Challenge 1
+# Make function to get population of all landlocked european countries using v3.1 restcountries api
+# API documentation: https://restcountries.com/
+# Make rest call using this link: https://restcountries.com/v3.1/subregion/europe
+
+def get_population_of_all_landlocked_european_countries():
+
+
+# Challenge 2
