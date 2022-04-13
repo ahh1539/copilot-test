8a718ecd6512aac59ccb01d07579f0fb7cedbe10
Austin <32467683+aushe02@users.noreply.github.com>
2022-04-13 13:35:36 -0400

    Update challenge5.rb

--- challenge5.rb
+++ challenge5.rb
@@ -6,10 +6,3 @@
 # Example: Given the input of 3, the function should return 2.
 # Example: Given the input of 8, the function should return 21.
 # Example: Given the input of 9, the function should return 34.
-def fibonacci(n)
-  if n == 1 || n == 2
-    return 1
-  else
-    return fibonacci(n-1) + fibonacci(n-2)
-  end
-end
