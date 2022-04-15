3a2f89d5070bfc04b57c7677fb6670bf9ead1aff
ahh1539 <ahh1539@g.rit.edu>
2022-04-14 21:08:02 -0400

    update to challenge 4

--- challenge4.sql
+++ challenge4.sql
@@ -1,3 +1,11 @@
 -- Create User table with id, name, email, and age, and insert 3 rows
 -- Make id the primary key and auto increment
 -- Make name and email unique
+
+
+
+
+
+
+-- Next create a table called posts with id, title, body, 
+-- and user_id where user_id is a foreign key to the users table
